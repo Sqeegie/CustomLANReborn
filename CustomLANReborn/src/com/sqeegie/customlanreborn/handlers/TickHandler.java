4d1dfda63f76a3f90e3b11fbbd8ef2ca3deb7d99
Sqeegie <jdwoodward74@yahoo.com>
2017-01-10 22:15:52 -0500

    Updated .gitignore.

--- CustomLANReborn/src/com/sqeegie/customlanreborn/handlers/TickHandler.java
+++ CustomLANReborn/src/com/sqeegie/customlanreborn/handlers/TickHandler.java
@@ -15,58 +15,52 @@ import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ChatComponentTranslation;
 
-public class TickHandler
-{
-  @SubscribeEvent
-  public void onTick(TickEvent.RenderTickEvent event)
-  {
-    if ((Minecraft.getMinecraft().currentScreen instanceof GuiShareToLan)) {
-      Minecraft.getMinecraft().displayGuiScreen(new GUIHandler(new GuiIngameMenu()));
-    } else if (!(Minecraft.getMinecraft().currentScreen instanceof GuiIngameMenu)) {}
-  }
-  
-  @SubscribeEvent
-  public void onServerTick(TickEvent.ServerTickEvent event)
-  {
-    if (MinecraftServer.getServer().isServerRunning()) {
-      try
-      {
-        String[] arr = Minecraft.getMinecraft().getIntegratedServer().getAllUsernames();
-        for (int i = 0; i < arr.length; i++)
-        {
-          EntityPlayerMP player = PlayerUtil.getPlayerByUsername(arr[i]);
-          String lastplayer = null;
-          if ((player.isDead) && (lastplayer != player.getDisplayName()))
-          {
-            lastplayer = player.getDisplayName();
-            try
-            {
-              File folder = new File(Minecraft.getMinecraft().mcDataDir + "/config/CustomLANReborn/lastPos/");
-              File posFile = new File(folder, player.getDisplayName() + ".pos");
-              Properties prop = new Properties();
-              if (!posFile.exists())
-              {
-                folder.mkdirs();
-                posFile.createNewFile();
-              }
-              FileOutputStream FOS = new FileOutputStream(posFile);
-              prop.setProperty("x", String.valueOf(player.posX));
-              prop.setProperty("y", String.valueOf(player.posY));
-              prop.setProperty("z", String.valueOf(player.posZ));
-              prop.setProperty("yaw", String.valueOf(player.cameraYaw));
-              prop.setProperty("pitch", String.valueOf(player.cameraPitch));
-              prop.store(FOS, null);
-              FOS.close();
-            }
-            catch (Exception e)
-            {
-              player.addChatMessage(new ChatComponentTranslation("Could not save last death location. /Back command will not be usable.", new Object[0]));
-              System.out.println(e);
-            }
-          }
-        }
-      }
-      catch (Exception localException1) {}
-    }
-  }
+public class TickHandler {
+	@SubscribeEvent
+	public void onTick(TickEvent.RenderTickEvent event) {
+		if ((Minecraft.getMinecraft().currentScreen instanceof GuiShareToLan)) {
+			Minecraft.getMinecraft().displayGuiScreen(new GUIHandler(new GuiIngameMenu()));
+		}
+		else if (!(Minecraft.getMinecraft().currentScreen instanceof GuiIngameMenu)) {
+		}
+	}
+
+	@SubscribeEvent
+	public void onServerTick(TickEvent.ServerTickEvent event) {
+		if (MinecraftServer.getServer().isServerRunning()) {
+			try {
+				String[] arr = Minecraft.getMinecraft().getIntegratedServer().getAllUsernames();
+				for (int i = 0; i < arr.length; i++) {
+					EntityPlayerMP player = PlayerUtil.getPlayerByUsername(arr[i]);
+					String lastplayer = null;
+					if ((player.isDead) && (lastplayer != player.getDisplayName())) {
+						lastplayer = player.getDisplayName();
+						try {
+							File folder = new File(Minecraft.getMinecraft().mcDataDir + "/config/CustomLANReborn/lastPos/");
+							File posFile = new File(folder, player.getDisplayName() + ".pos");
+							Properties prop = new Properties();
+							if (!posFile.exists()) {
+								folder.mkdirs();
+								posFile.createNewFile();
+							}
+							FileOutputStream FOS = new FileOutputStream(posFile);
+							prop.setProperty("x", String.valueOf(player.posX));
+							prop.setProperty("y", String.valueOf(player.posY));
+							prop.setProperty("z", String.valueOf(player.posZ));
+							prop.setProperty("yaw", String.valueOf(player.cameraYaw));
+							prop.setProperty("pitch", String.valueOf(player.cameraPitch));
+							prop.store(FOS, null);
+							FOS.close();
+						}
+						catch (Exception e) {
+							player.addChatMessage(new ChatComponentTranslation("Could not save last death location. /Back command will not be usable.", new Object[0]));
+							System.out.println(e);
+						}
+					}
+				}
+			}
+			catch (Exception localException1) {
+			}
+		}
+	}
 }
